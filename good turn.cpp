cfb3d24ab59986654da1409ea813cc737a5acb99
Deepak Gupta <123310457+DeepakGupta0001@users.noreply.github.com>
2023-07-22 16:59:26 +0530

    Add files via upload

---
+++ good turn.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int x,y;
+cout<<"Enter the number which shown on first dice : ";
+    cin>>x;
+    cout<<"Enter the number which shown on second dice : ";
+    cin>>y;
+    if(x+y>6)
+    cout<<"YES"<<endl;
+    else
+    cout<<"NO"<<endl;
+
+	return 0;
+}
