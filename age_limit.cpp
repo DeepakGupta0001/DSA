ea06c4211c6fb8451ec5698e27ec98dcd3758a8a
Deepak Gupta <123310457+DeepakGupta0001@users.noreply.github.com>
2023-07-24 21:38:27 -0700

    Add files via upload

---
+++ age_limit.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+        int x,y,a;
+cout<<"enter your age : ";
+cin>>a;
+cout<<"enter the age from which your age is strictly lesser : ";
+cin>>y;
+cout<<"enter the age from which your age is equal or greater : ";
+cin>>a;
+
+
+        if(x<=a && y>a)
+        cout<<"YES"<<endl;
+        else
+        cout<<"NO"<<endl;
+    
+	// your code goes here
+	return 0;
+}
