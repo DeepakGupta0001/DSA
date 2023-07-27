dcf3a30b9e08ceda66b399847f6ef3a6287f4721
Deepak Gupta <123310457+DeepakGupta0001@users.noreply.github.com>
2023-07-26 23:50:40 -0700

    Add files via upload

---
+++ squats.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){int x,y,n;
+cout<<"enter number of sets of squats : ";
+cin>>x;
+cout<<"enter number of squats in one sets : ";
+cin>>y;
+cout<<"total number of squats : "<<y*x;}
